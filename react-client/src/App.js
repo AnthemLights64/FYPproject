e667aa9b8d53868903a4907b3412d07cedb64700
Wenlong Lu <yummybroccoli@outlook.com>
2021-01-22 14:43:07 +0000

    import antd, on-demand packaging, custom themes

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button, message } from "antd";
+import './App.less';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+  
+  handleClick = () => {
+    message.success("Success!");
+  }
+
+  render () {
+    return <Button type="primary" onClick={this.handleClick}>Primary</Button>
+  }
+
 }
-
-export default App;
