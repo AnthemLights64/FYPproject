8da2e5c4a97fa94db9c030a9b609ce39793ce80e
Wenlong Lu <yummybroccoli@outlook.com>
2021-01-25 13:11:45 +0000

    structure of login page constructed

--- react-client/src/pages/login/login.jsx
+++ react-client/src/pages/login/login.jsx
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
+import './login.less';
+import logo from './images/logo.jpg';
+
 export default class Login extends Component {
     render () {
         return (
-            <div>Login</div>
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>RNG E-Sports Club</h1>
+                </header>
+                <section className="login-content">
+                    <h2>Login</h2>
+                    <div>Form Component</div>
+                </section>
+            </div>
         );
     }
 }
