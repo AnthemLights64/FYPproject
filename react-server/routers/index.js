30edc981b5ff9bc22d31b5b6f56e6e93e18a5224
Wenlong Lu <yummybroccoli@outlook.com>
2021-02-13 22:30:45 +0000

    paging function implemented

--- react-server/routers/index.js
+++ react-server/routers/index.js
@@ -133,3 +133,26 @@ router.post('/manage/role/update', (req, res) => {
             res.send({status: 1, msg: 'Update role exception. Please try again.'});
         });
 });
+
+// Get the paging information object of the specified array
+function pageFilter(arr, pageNum, pageSize) {
+    pageNum = pageNum * 1;
+    pageSize = pageSize * 1;
+    const total = arr.length;
+    const pages = Math.floor((total + pageSize - 1) / pageSize);
+    const start = pageSize * (pageNum - 1);
+    const end = start + pageSize <= total ? start + pageSize : total;
+    const list = [];
+    for (var i=start; i<end; i++) {
+        list.push(arr[i]);
+    }
+    return {
+        pageNum,
+        total,
+        pages,
+        pageSize,
+        list
+    }
+}
+
+module.exports = router;
