23c6485a3f610857b4d45c85a560b77d9bbed798
Wenlong Lu <yummybroccoli@outlook.com>
2021-03-01 19:43:55 +0000

    route jumps of levtNav

--- react-client/src/components/left-nav/index.jsx
+++ react-client/src/components/left-nav/index.jsx
@@ -25,19 +25,37 @@ export default class LeftNav extends Component {
                         mode="inline"
                         theme="dark"
                         >
-                        <Menu.Item key="1" icon={<HomeOutlined />}>
-                            Homepage: Team
+                        <Menu.Item key="/home" icon={<HomeOutlined />}>
+                            <Link to='/home'>
+                                Homepage: Team
+                            </Link>
                         </Menu.Item>
-                        <Menu.Item key="2" icon={<ScheduleOutlined />}>
-                            Calendar
+                        <Menu.Item key="/calendar" icon={<ScheduleOutlined />}>
+                            <Link to='/calendar'>
+                                Calendar
+                            </Link>
                         </Menu.Item>
-                        <Menu.Item key="3" icon={<TeamOutlined />}>
-                            Members
+                        <Menu.Item key="/member" icon={<TeamOutlined />}>
+                            <Link to='/member'>
+                                Members
+                            </Link>
                         </Menu.Item>
-                        <SubMenu key="sub" icon={<BarsOutlined />} title="Management">
-                            <Menu.Item key="4" >Members Management</Menu.Item>
-                            <Menu.Item key="5" >User Management</Menu.Item>
-                            <Menu.Item key="6" >Role Management</Menu.Item>
+                        <SubMenu key="Management" icon={<BarsOutlined />} title="Management">
+                            <Menu.Item key="/management/member" >
+                                <Link to='/management/member'>
+                                    Members Management
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="/management/user" >
+                                <Link to='/management/user'>
+                                    User Management
+                                </Link>
+                            </Menu.Item>
+                            <Menu.Item key="/management/role" >
+                                <Link to='/management/role'>
+                                    Role Management
+                                </Link>
+                            </Menu.Item>
                         </SubMenu>
                         
                     </Menu>
