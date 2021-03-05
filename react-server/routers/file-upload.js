4deb9bad65aaf1aff5aff62926678961a32b63db
Wenlong Lu <yummybroccoli@outlook.com>
2021-03-05 22:47:03 +0000

    router of uploading file and deleting file

---
+++ react-server/routers/file-upload.js
@@ -0,0 +1,67 @@
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const dirPath = path.join(__dirname, '..', 'public/upload');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdir(dirPath, function (error) {
+                if (error) {
+                    console.log(err);
+                } else {
+                    cb(null, dirPath);
+                }
+            });
+        } else {
+            cb(null, dirPath);
+        }
+    },
+    filename: function (req, file, cb) {
+        var ext = path.extname(file.originalname);
+        cb(null, file.fieldname + '-' + Date.now() + ext);
+    }
+});
+const upload = multer({storage});
+const uploadOne = upload.single('image');
+
+module.exports = function fileUpload(router) {
+    // Upload an image
+    router.post('management/image/upload', (req, res) => {
+        uploadOne(req, res, function (error) {
+            if (error) {
+                return res.send({
+                    status: 1,
+                    msg: 'Failed to upload file.'
+                });
+            }
+            var file = req.file;
+            res.send({
+                status: 0,
+                data: {
+                    name: file.filename,
+                    url: '#/upload/' + filename.filename
+                }
+            });
+        });
+    });
+
+    // Delete an image
+    router.post('/management/image/delete', (req, res) => {
+        const {name} = req.body;
+        fs.unlink(path.join(dirPath, name), (err) => {
+            if (err) {
+                console.log(err);
+                res.send({
+                    status: 1,
+                    msg: 'Failed to delete file.'
+                });
+            } else {
+                res.send({
+                    status: 0
+                });
+            }
+        });
+    });
+}
