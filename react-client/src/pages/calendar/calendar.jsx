5fa6e0a1cfaefffc032932fdef9f7d58f3cc3422
Wenlong Lu <yummybroccoli@outlook.com>
2021-04-07 00:29:30 +0800

    layout of adding new event

--- react-client/src/pages/calendar/calendar.jsx
+++ react-client/src/pages/calendar/calendar.jsx
@@ -1,75 +1,123 @@
 import React, {Component} from 'react';
-import { Calendar, Badge } from 'antd';
+import { Calendar, Badge, Alert, Button, Modal } from 'antd';
+import moment from 'moment';
+import { formatDate } from '../../utils/dateUtils';
+import EventForm from './event-form';
 
 export default class TeamCalendar extends Component {
 
-    
-    render () {
+  state = {
+    value: moment(formatDate(Date.now())),
+    selectedValue: moment(formatDate(Date.now())),
+    isShownEventForm: false
+  };
 
-        function getListData(value) {
-            let listData;
-            switch (value.date()) {
-              case 8:
-                listData = [
-                  { type: 'warning', content: 'This is warning event.' },
-                  { type: 'success', content: 'This is usual event.' },
-                ];
-                break;
-              case 10:
-                listData = [
-                  { type: 'warning', content: 'This is warning event.' },
-                  { type: 'success', content: 'This is usual event.' },
-                  { type: 'error', content: 'This is error event.' },
-                ];
-                break;
-              case 15:
-                listData = [
-                  { type: 'warning', content: 'This is warning event' },
-                  { type: 'success', content: 'This is very long usual event。。....' },
-                  { type: 'error', content: 'This is error event 1.' },
-                  { type: 'error', content: 'This is error event 2.' },
-                  { type: 'error', content: 'This is error event 3.' },
-                  { type: 'error', content: 'This is error event 4.' },
-                ];
-                break;
-              default:
-            }
-            return listData || [];
-          }
-          
-          function dateCellRender(value) {
-            const listData = getListData(value);
-            return (
-              <ul className="events">
-                {listData.map(item => (
-                  <li key={item.content}>
-                    <Badge status={item.type} text={item.content} />
-                  </li>
-                ))}
-              </ul>
-            );
-          }
-          
-          function getMonthData(value) {
-            if (value.month() === 8) {
-              return 1394;
-            }
-          }
-          
-          function monthCellRender(value) {
-            const num = getMonthData(value);
-            return num ? (
-              <div className="notes-month">
-                <section>{num}</section>
-                <span>Backlog number</span>
-              </div>
-            ) : null;
-          }
-          
-            
-        
+  onSelect = value => {
+    this.setState({
+      value,
+      selectedValue: value
+    });
+  }
+    
+  onPanelChange = value => {
+    this.setState({
+      value
+    });
+  }
+
+  render () {
+
+    const { value, selectedValue, isShownEventForm } = this.state;
+
+    function getListData(value) {
+        let listData;
+        switch (value.date()) {
+          case 8:
+            listData = [
+              { type: 'warning', content: 'This is warning event.' },
+              { type: 'success', content: 'This is usual event.' },
+            ];
+            break;
+          case 10:
+            listData = [
+              { type: 'warning', content: 'This is warning event.' },
+              { type: 'success', content: 'This is usual event.' },
+              { type: 'error', content: 'This is error event.' },
+            ];
+            break;
+          case 15:
+            listData = [
+              { type: 'warning', content: 'This is warning event' },
+              { type: 'success', content: 'This is very long usual event。。....' },
+              { type: 'error', content: 'This is error event 1.' },
+              { type: 'error', content: 'This is error event 2.' },
+              { type: 'error', content: 'This is error event 3.' },
+              { type: 'error', content: 'This is error event 4.' },
+            ];
+            break;
+          default:
+        }
+        return listData || [];
+      }
+      
+      function dateCellRender(value) {
+        const listData = getListData(value);
         return (
-            <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+          <ul className="events">
+            {listData.map(item => (
+              <li key={item.content}>
+                <Badge status={item.type} text={item.content} />
+              </li>
+            ))}
+          </ul>
         );
-    }
+      }
+      
+      function getMonthData(value) {
+        if (value.month() === 8) {
+          return 1394;
+        }
+      }
+      
+      function monthCellRender(value) {
+        const num = getMonthData(value);
+        return num ? (
+          <div className="notes-month">
+            <section>{num}</section>
+            <span>Backlog number</span>
+          </div>
+        ) : null;
+      }
+      
+        
+    
+    return (
+      <>
+        <Alert message={`You selected date: ${selectedValue && selectedValue.format('YYYY-MM-DD')}`}/>
+        <Button type='primary' disabled={false} onClick={() => this.setState({isShownEventForm: true})}>Edit Events</Button>
+        <Calendar 
+          dateCellRender={dateCellRender} 
+          monthCellRender={monthCellRender} 
+          value={value}
+          onSelect={this.onSelect}
+          onPanelChange={this.onPanelChange}
+        />
+        <Modal
+          title='Edit Event'
+          visible={isShownEventForm}
+          //onOk={}
+          onCancel={() => {
+              this.setState({
+                isShownEventForm: false
+              });
+              this.form.current.resetFields();
+          }}
+      >
+          <EventForm
+              setForm={(form) => this.form = form}
+          />
+      </Modal>  
+      </>
+    );
+  }
 }
---
+++ react-client/src/pages/calendar/event-form.jsx
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { Form, Input, Button } from 'antd';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import './event-form.less';
+
+const {List, Item} = Form;
+
+export default class EventForm extends Component {
+
+    formRef = React.createRef();
+
+    static propTypes = {
+        setForm: PropTypes.func.isRequired,
+    }
+
+    UNSAFE_componentWillMount () {
+        this.props.setForm(this.formRef);
+    }
+
+    render() {
+
+        const formItemLayout = {
+            labelCol: {
+              xs: { span: 24 },
+              sm: { span: 4 },
+            },
+            wrapperCol: {
+              xs: { span: 24 },
+              sm: { span: 20 },
+            },
+        };
+
+        const formItemLayoutWithOutLabel = {
+            wrapperCol: {
+              xs: { span: 24, offset: 0 },
+              sm: { span: 20, offset: 4 },
+            },
+        };
+
+        return (
+            <Form ref={this.formRef} {...formItemLayoutWithOutLabel}>
+                <List 
+                    name="events"
+ 
+                >
+                    {(fields, { add, remove }, { errors }) => (
+                        <>
+                            {fields.map((field, index) => (
+                                <Item
+                                    {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                                    label={index === 0 ? 'Events' : ''}
+                                    required={false}
+                                    key={field.key}
+                                >
+                                    <Item
+                                        {...field}
+                                        validateTrigger={['onChange', 'onBlur']}
+                                        rules={[
+                                            {
+                                            required: true,
+                                            whitespace: true,
+                                            message: "Please input the event or delete this field.",
+                                            },
+                                        ]}
+                                        noStyle
+                                    >
+                                        <Input placeholder="Event" style={{ width: '60%' }} />
+                                    </Item>
+                                    {fields.length > 1 ? (
+                                        <MinusCircleOutlined
+                                            className="dynamic-delete-button"
+                                            onClick={() => remove(field.name)}
+                                        />
+                                    ) : null}
+                                </Item>
+                            ))}
+                            <Item>
+                                <Button
+                                    type="dashed"
+                                    onClick={() => add()}
+                                    style={{ width: '60%' }}
+                                    icon={<PlusOutlined />}
+                                >
+                                    Add new event
+                                </Button>
+                                <Form.ErrorList errors={errors} />
+                            </Item>
+                        </>
+                    )}
+                </List>
+            </Form>
+        );
+    }
+}
