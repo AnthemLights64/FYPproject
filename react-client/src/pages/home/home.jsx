39fb6aa5d2f2004104298f0c1e1264e850bcf5f6
Wenlong Lu <yummybroccoli@outlook.com>
2021-04-12 02:06:32 +0800

    homepage

--- react-client/src/pages/home/home.jsx
+++ react-client/src/pages/home/home.jsx
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import './home.less';
 
 export default class Home extends Component {
     render () {
         return (
-            <div>
-                Home
-            </div>
+            <>
+                <div className="levels">
+                    <div className="level one">
+                        <div className="title">LPL Spring</div>
+                        <div className="content">Best record: Champion</div>
+                    </div>
+                    <div className="level two">
+                        <div className="title">LPL Summer</div>
+                        <div className="content">Best record: Champion</div>
+                    </div>
+                    <div className="level three">
+                        <div className="title">MSI</div>
+                        <div className="content">Best record: Champion</div>
+                    </div>
+                    <div className="level four">
+                        <div className="title">World</div>
+                        <div className="content">Best record: Runner-up</div>
+                    </div>
+                </div>
+            </>
+            
         );
     }
 }
