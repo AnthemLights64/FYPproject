4b53157c7d05537b017f595c19c93c633918d43a
Wenlong Lu <yummybroccoli@outlook.com>
2021-04-26 06:12:06 +0800

    LPL Spring page

--- react-client/src/pages/home/LPLSpring.jsx
+++ react-client/src/pages/home/LPLSpring.jsx
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import { Button, Card } from 'antd';
 import {ArrowLeftOutlined} from '@ant-design/icons';
+import './style.less'
+
+import img1 from '../../assets/images/Spring2021.jpg';
+import img2 from '../../assets/images/RNGcurtain.png';
+import img3 from '../../assets/images/photo2021.jpg';
 
 export default class LPLSpring extends Component {
     render () {
@@ -20,8 +25,46 @@ export default class LPLSpring extends Component {
         return (
             <>
                 <Card title={title}>
-                    LPLSpring
+                    <div className="row">
+                        <div className="col-lg-10 m-auto">
+                            <div>
+                                <h5>Champion</h5>
+                                <h1>A joint effort by all</h1>
+                                <p>This is the third spring championship of RNG in the LPL. This time, RNG won the final championship by beating 3 players in the group of defeaters in the playoffs.</p>
+                            </div>
+                        </div>
+                    </div>   
+                    <div>
+                        <div style={{textAlign: 'center', marginTop: 20}}>
+                            <img style={{maxWidth: 700}} src={img1} alt=""/>
+                        </div>
+                        <div>
+                            <div>
+                                <p class="p-content">CONGRATULATIONS to RNG for winning the 2021 LPL Spring Championship with a 3-1 victory over FPX! They will also represent LPL at MSI 2021!</p>
+                                <p>This spring, RNG shouldered all the expectations and doubts, and rode the waves. </p><p>A season of hard work and hard work has resulted in endless surprises, and although we have been in the abyss, we have never hit rock bottom. </p><p>Today, RNG is shining, just to watch the gold fall across the sky again. </p><p>The RNG are announcing to the world that their journey is much more than that, that they are aiming for the sky and that their ambition is to go far!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div style={{flex: 10, maxWidth: 500, margin: 30}}>
+                                <div class="image-v">
+                                    <img src={img2} alt=""/>
+                                </div>
+                            </div>
+                            <div style={{flex: 10, maxWidth: 500, margin: 30}}>
+                                <div class="image-v">
+                                    <img src={img3} alt=""/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                                <blockquote className="blockquote">
+                                This is the power of the new generation of juniors and we expect and aspire to better results.
+                                </blockquote>
+                        </div>
+                    </div>
                 </Card>
+                
             </>
         );
     }
